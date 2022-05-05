fa5c0e01d403151a5430d2a0b7bffbf2fe1ce81e
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-05-05 15:33:55 +0530

    Add: welcome message

---
+++ main/StateCensusAnalyser.java
@@ -0,0 +1,9 @@
+package indianstatescensusanalyser;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to State Census Analyser Program");
+	}
+
+}
